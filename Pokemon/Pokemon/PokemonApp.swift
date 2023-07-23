8c90dbbf1f8f755d94cbf13202b54aa577a4bb9a
Kerim Dzaninjuhovic <kerim.dzaninjuhovic@vitecsoftware.com>
2023-07-23 11:53:34 +0200

    added initial xcode project

---
+++ Pokemon/Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Kerim Njuhovic on 23. 7. 2023..
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
